a94de76b2dcc38ba777cdb301062cf55de83f167
tbro-dev <tyler.broussard@live.com>
2025-10-03 12:39:36 -0700

    build: prepare for production deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import Navigation from './component/Navigation';
-import Header from './component/Header';
-import Portfolio from './component/Portfolio';
-import Services from './component/Services';
-import About from './component/About';
+import Navigation from '@/component/Navigation';
+import Header from '@/component/Header';
+import Portfolio from '@/component/Portfolio';
+import Services from '@/component/Services';
+import About from '@/component/About';
 
 
 export default function HomePage() {
   return (
     <>
-      <Navigation />
-      <Header />
-      <Portfolio />
-      <Services />
-      <About />
+      <div>
+        <Navigation />
+        <Header />
+        <Portfolio />
+        <Services />
+        <About />
+      </div>
     </>
   );
 }
