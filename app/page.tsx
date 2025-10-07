d08af649d673eed4b95e835d8400c4b970e2a286
tbro-dev <tyler.broussard@live.com>
2025-10-07 12:37:44 -0700

    feat: add ComingSoonHero to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
-import Navigation from '@/component/Navigation';
-import Header from '@/component/Header';
-import Portfolio from '@/component/Portfolio';
-import Services from '@/component/Services';
-import About from '@/component/About';
-import Footer from '@/component/Footer';
-import Contact from '@/component/Contact';
-import { Container } from '@mui/material';
+import ComingSoonHero from "@/component/ComingSoonHero";
 
 
 export default function HomePage() {
   return (
-    <Container>
-     <div >        
-        <div >
-          <div>          
-            <div>Busy Making Media</div>
-            <h1>Busy Making Media</h1>
-            <p>Services coming soon. Shoot us an email if you're curious.</p>
-          </div>
-          <a href="mailto:tyler@busymakingmedia.onmicrosoft.com">
-            <div className="cta">Send us an email</div>
-          </a>
-        </div>        
-      </div>
-    </Container>
+    <>
+    <ComingSoonHero />
+    </>
   );
 }
